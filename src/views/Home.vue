70b182c9c5723dd7e6694d841530dde9527ad971
ashikurweb <ashikurrahman7194@gmail.com>
2025-10-30 22:56:20 +0600

    setup video downloader

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-600">Hello World</h1>
+  </div>
+ </template>
+
+<script setup>
+</script>
+
+<style scoped>
+</style>
+
+
